688ff0e757fc4bc69ecd81c723ac9dfb8169ea01
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-04-03 22:51:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TextEditor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("O que voce deseja fazer?");
+      Console.WriteLine("1 - Abrir arquivo");
+      Console.WriteLine("2 - Criar novo arquivo");
+      Console.WriteLine("0 - Sair");
+      short option = short.Parse(Console.ReadLine());
+
+      switch (option)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+      }
+    }
+    static void Abrir() { }
+    static void Editar()
+    {
+      Console.Clear();
+      Console.WriteLine("Digite seu texto abaixo");
+    }
+  }
+}
